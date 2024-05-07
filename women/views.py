6d04728897d732dcff3094590bbef7de2c132627
BalashovDV <korochun@yandex.ru>
2024-05-07 14:48:06 +0300

    Изучаем Джанго (Django). Добавлена передача данных в шаблоны страниц.

--- women/views.py
+++ women/views.py
@@ -2,13 +2,31 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'women/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def page_not_found(request, exception):
