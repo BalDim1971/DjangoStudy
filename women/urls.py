1ae14d8e352d121f1639aa786141b56dcf2ccab4
BalashovDV <korochun@yandex.ru>
2024-05-10 08:31:15 +0300

    Изучаем Джанго (Django). Шаблонный тег url. Изменено отображение меню в вертикальный вид. Добавлены ссылки на пункты меню для отображения следующих страниц.

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from women.views import index, about
+from women.views import index, about, show_post, addpage, contact, login
 
 urlpatterns = [
     path('', index, name='home'),  # http://127.0.0.1:8000/
     path('about/', about, name='about'),  # http://127.0.0.1:8000/about
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post')
 ]
--- women/views.py
+++ women/views.py
@@ -1,13 +1,22 @@
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
 from django.template.defaultfilters import slugify
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
-
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+    {'id': 1, 'title': 'Анджелина Джоли',
+     'content': 'Биография Анджелины Джоли',
+     'is_published': True},
+    {'id': 2, 'title': 'Марго Робби',
+     'content': 'Биография Марго Робби',
+     'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс',
+     'content': 'Биография Джулия Робертс',
+     'is_published': True},
 ]
 
 
@@ -40,3 +49,19 @@ def about(request):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
