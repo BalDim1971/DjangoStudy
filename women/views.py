bd9291073f9bb8ee0ba2bf8e52eff6ed1d243258
BalashovDV <korochun@yandex.ru>
2024-05-10 07:39:27 +0300

    Изучаем Джанго (Django). Добавлены теги for и if. Просмотр данных словаря.

--- women/views.py
+++ women/views.py
@@ -4,6 +4,12 @@ from django.template.defaultfilters import slugify
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
 
 class MyClass:
     def __init__(self, a, b):
@@ -14,10 +20,11 @@ class MyClass:
 # Create your views here.
 def index(request):
     data = {
-        'title': 'главная страница',
+        'title': 'Главная страница',
         'main_title': 'main_title',
         'url': slugify("The Main Title 1"),
         'menu': menu,
+        'posts': data_db,
         'float': 28.56,
         'lst': [1, 2, 'abc', True],
         'set': {1, 1, 2, 3, 2, 5},
