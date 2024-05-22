c55ca91f8f7c5991cc142404fcd44ca0ea95cad1
BalashovDV <korochun@yandex.ru>
2024-05-22 07:30:31 +0300

    Изучаем Джанго (Django). https://proproprogs.ru/django4 Добавлен урок 41 включительно. Экспериментируем с админ-панелью. Добавлено формирование поля slug на лету (prepopulated_fields). Добавляем ввод тэгов с выбором из списков. В модели переименовано поле slug.

--- women/models.py
+++ women/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.urls import reverse
+
+
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+    
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
 
 
 class PublishedModel(models.Manager):
@@ -16,7 +27,7 @@ class Women(models.Model):
 
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, db_index=True, unique=True,
-                            verbose_name='URL')
+                            verbose_name='Slug')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     time_create = models.DateTimeField(auto_now_add=True,
                                        verbose_name='Время создания')
@@ -58,6 +69,11 @@ class Women(models.Model):
 
     def __str__(self):
         return self.title
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
+    #
 
 
 class Husband(models.Model):
